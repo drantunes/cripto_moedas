dc612b414878b0c9474b540c7b7ee824281418e7
Diego Roberto Antunes <drantunes@gmail.com>
2021-07-13 19:12:55 -0300

    Setup do Firebase no Projeto conforme documentação

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:cripto_moedas/configs/app_settings.dart';
 import 'package:cripto_moedas/configs/hive_config.dart';
 import 'package:cripto_moedas/repositories/conta_repository.dart';
 import 'package:cripto_moedas/repositories/favoritas_repository.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'meu_aplicativo.dart';
@@ -9,6 +10,7 @@ import 'meu_aplicativo.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await HiveConfig.start();
+  await Firebase.initializeApp();
 
   runApp(
     MultiProvider(
--- lib/meu_aplicativo.dart
+++ lib/meu_aplicativo.dart
@@ -1,6 +1,5 @@
 import 'package:cripto_moedas/pages/home_page.dart';
 import 'package:flutter/material.dart';
-import 'package:cripto_moedas/pages/moedas_page.dart';
 
 class MeuAplicativo extends StatelessWidget {
   const MeuAplicativo({Key? key}) : super(key: key);
